8b3b86f0299e42b2619c89f978f8223cbabb3171
shendo <steve.henderson@hendotech.com.au>
2021-03-10 01:09:57 +1100

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -47,9 +47,8 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Security'
     ],
